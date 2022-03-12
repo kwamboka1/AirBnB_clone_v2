0ca36699c653de214c9209eb45f18e23c3b5788f
kwamboka1 <makorimildred@gmail.com>
2022-03-12 01:32:00 +0000

    [Fix]:code

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -6,15 +6,17 @@ from models.amenity import Amenity
 
 
 class test_Amenity(test_basemodel):
-    """ """
+    """ Tests amenity """
 
     def __init__(self, *args, **kwargs):
-        """ """
+        
+        """ Tests to check all arguments are working"""
         super().__init__(*args, **kwargs)
         self.name = "Amenity"
         self.value = Amenity
 
     def test_name2(self):
-        """ """
+        
+        """ tests for secnd name """
         new = self.value()
         self.assertEqual(type(new.name), str)
