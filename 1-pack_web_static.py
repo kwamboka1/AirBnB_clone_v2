9b62e4bf01c9cb42303657a36acd084724e83561
kwamboka1 <makorimildred@gmail.com>
2022-03-25 14:55:41 +0300

    Update 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
-""" Function that compresses a folder """
+"""Fabric script that generates a .tgz archive
+from the contents of the web_static folder"""
 
+from fabric.operations import local
 from datetime import datetime
-from fabric.api import local
-from  os.path import isdir 
 
 
-"""A function that generates a tgz archive"""
 def do_pack():
-    try:
-        date = datetime.now().strftime("%Y%m%d%H%M%S")
-        if isdir("versions") is False:
-            local('mkdir versions')
-        file_name = "versions/web_static_{}.tgz".format(date)
-        local('tar -cvzf {} web_static'.format(file_name))
-        return file_name
-    except:
+    """Function to compress files"""
+    local("mkdir -p versions")
+    result = local("tar -cvzf versions/web_static_{}.tgz web_static"
+                   .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")),
+                   capture=True)
+    if result.failed:
         return None
+    return result
